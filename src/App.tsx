57cbcbc68ec80aa3c1c5c78507b5fbcdd27951a6
stitaju <sirish.titaju@fusemachines.com>
2025-04-28 17:10:41 +0545

    fix: copyrights

--- src/App.tsx
+++ src/App.tsx
@@ -115,6 +115,17 @@ function App() {
           listItemRefs={listItemRefs}
         />
       )}
+      <p className="flex items-center gap-2 absolute bottom-[7rem] right-[-3rem] rotate-270">
+        <div className="line border-b-2 border-amber-50  w-[55px]"></div>
+        Developed By:{' '}
+        <a
+          href="https://sirishtitaju.com.np/"
+          target="_blank"
+          className="font-medium hover:text-blue-200"
+        >
+          SIRISH
+        </a>
+      </p>
     </section>
   );
 }
